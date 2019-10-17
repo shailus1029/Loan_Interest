a78a58415860565213158543ce7b3041b68a85d4
shailus1029 <shailus1029@gmail.com>
2019-10-17 07:28:21 +0530

    side bar component in progress

--- src/Components/Molecules/Sidebar/Sidebar.jsx
+++ src/Components/Molecules/Sidebar/Sidebar.jsx
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
 import "./Sidebar.css";
+import { List, Avatar } from "antd";
+import "antd/dist/antd.css";
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          title: "Ant Design Title 1"
+        },
+        {
+          title: "Ant Design Title 2"
+        },
+        {
+          title: "Ant Design Title 3"
+        },
+        {
+          title: "Ant Design Title 4"
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div className="sidebarContainer">
-        <div>History Module</div>
+        <div className="historyheader">History Module</div>
+        <List
+          itemLayout="horizontal"
+          dataSource={this.state.data}
+          renderItem={item => (
+            <List.Item>
+              <div className="singleHistory">
+                <div className="loanheading">
+                  <p className="subData1">Loan Amount</p>
+                  <p className="subData2">Durations</p>
+                </div>
+                <div className="loanheading">
+                  <p className="subData1">Interest</p>
+                  <p className="subData2">Total Amount</p>
+                </div>
+              </div>
+            </List.Item>
+          )}
+        />
+        <div className="historyContent"></div>
       </div>
     );
   }
